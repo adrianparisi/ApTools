fac9be006dbda53fb0ee2817f139be123b1fcea4
adrianparisi <adrian.parisi@gmail.com>
2015-04-11 18:20:32 -0300

    Refactor and chenges on ExceptionLogger.

---
+++ SomeTools.Tests/ExceptionLoggerTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+
+namespace SomeTools.Tests
+{
+    [TestClass]
+    public class ExceptionLoggerTest
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.RemoveExistingLog();
+        }
+
+        private void RemoveExistingLog()
+        {
+            if (File.Exists(ExceptionLogger.FullName))
+            {
+                File.SetAttributes(ExceptionLogger.FullName, FileAttributes.Normal);
+                File.Delete(ExceptionLogger.FullName);
+
+                if (File.Exists(ExceptionLogger.FullName))
+                    throw new Exception("Can't remove error log.");
+            }
+        }
+
+
+        [TestMethod]
+        public void LogAndReadAnExceptionTest()
+        {
+            ExceptionLogger.Log(new Exception("some messagge"));
+            byte[] log = File.ReadAllBytes(ExceptionLogger.FullName);
+            
+            Assert.IsTrue(log.Count() > 0);
+        }
+
+
+        [TestMethod]
+        public void AppendASecondException()
+        {
+            ExceptionLogger.Log(new Exception("messagge 1"));
+            byte[] log1 = File.ReadAllBytes(ExceptionLogger.FullName);
+
+            ExceptionLogger.Log(new Exception("messagge 2"));
+            byte[] log2 = File.ReadAllBytes(ExceptionLogger.FullName);
+
+            Assert.IsTrue(log1.Count() > 0, "First log dosent contain information.");
+            Assert.IsTrue(log2.Count() > 0, "Second log dosent contain information.");
+            Assert.IsTrue(log1.Count() < log2.Count(), "First file is bigger than second file.");
+        }
+
+        [TestMethod]
+        public void CantOpenFileAndNotFail()
+        {
+            ExceptionLogger.Log(new Exception("messagge"));
+            byte[] log1 = File.ReadAllBytes(ExceptionLogger.FullName);
+
+            File.SetAttributes(ExceptionLogger.FullName, FileAttributes.ReadOnly);
+
+            ExceptionLogger.Log(new Exception("unwritten message"));
+            byte[] log2 = File.ReadAllBytes(ExceptionLogger.FullName);
+
+            Assert.AreEqual(log1.Count(), log2.Count());
+        }
+    }
+}
---
+++ SomeTools/ExeptionLogger.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SomeTools
+{
+    public class ExceptionLogger
+    {
+        public static string Path
+        {
+            get { return Application.StartupPath + "\\logs"; }
+        }
+
+        public static string FileName
+        {
+            get { return "error.log"; }
+        }
+
+        /// <summary>
+        /// Gets the path with the file name.
+        /// </summary>
+        public static string FullName
+        {
+            get { return Path + "\\" + FileName; }
+        }
+        
+
+        /// <summary>
+        /// Logs the exception.
+        /// </summary>
+        public static void Log(Exception exception)
+        {
+            try
+            {
+                if (!Directory.Exists(Path))
+                    Directory.CreateDirectory(Path);
+                
+                StreamWriter writer = null;
+
+                if (!File.Exists(FullName))
+                {
+                    FileStream stream = File.Create(FullName);
+                    stream.Close();
+
+                    writer = File.AppendText(FullName);
+                }
+                else
+                {
+                    writer = File.AppendText(FullName);
+                    writer.WriteLine();
+                    writer.WriteLine();
+                }
+
+                writer.WriteLine(DateTime.Now);
+                writer.WriteLine(exception.ToString());
+                writer.Flush();
+                writer.Close();
+            }
+            catch
+            {
+                // you can do nothing more.
+            }
+        }
+
+        /// <summary>
+        /// Handleds the unhandled exceptions.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="UnhandledExceptionEventArgs"/> instance containing the event data.</param>
+        public static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception exception = (Exception)e.ExceptionObject;
+
+            ExceptionLogger.Log(exception);
+            MessageBox.Show("Error: " + exception.Message, "Fatal error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
