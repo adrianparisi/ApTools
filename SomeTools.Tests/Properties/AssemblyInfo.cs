6ee056e038d77c8a7001f58946067894bd880f1c
adrianparisi <adrian.parisi@gmail.com>
2015-04-14 23:10:45 -0300

    Change project title on both assemblies.

--- SomeTools.Tests/Properties/AssemblyInfo.cs
+++ SomeTools.Tests/Properties/AssemblyInfo.cs
@@ -1,18 +1,17 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ApTools.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ApTools.Tests")]
+[assembly: AssemblyTitle("SomeTools.Tests")]
+//[assembly: AssemblyDescription("")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SomeTools.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
