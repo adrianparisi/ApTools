ebc7e1d4dc807d8e98e4f6c380fbbc3ed954edd1
Adrian Parisi <adrian.parisi@gmail.com>
2015-12-12 15:45:07 -0300

    Add AutoSize extension method to ComboBox.

---
+++ SomeTools.Tests/ExtensionMethods/ComboBoxExtensionMethodsTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
+
+namespace SomeTools.Tests.ExtensionMethods
+{
+    [TestClass]
+    public class ComboBoxExtensionMethodsTest
+    {
+        [TestMethod]
+        public void SmallIsSmallerThanExtraLargeTest()
+        {
+            ComboBox combo1 = new ComboBox();
+            combo1.Items.Add("small");
+            combo1.AutoSize();
+
+
+            ComboBox combo2 = new ComboBox();
+            combo2.Items.Add("extra large");
+            combo2.AutoSize();
+
+            Assert.IsTrue(combo1.Width < combo2.Width);
+        }
+
+        [TestMethod]
+        public void RecalculateSizeAfterAddItemsTest()
+        {
+            ComboBox combo = new ComboBox();
+            combo.Items.Add("small");
+            combo.AutoSize();
+
+            int width = combo.Width;
+
+            combo.Items.Add("extra large");
+            combo.AutoSize();
+
+            Assert.IsTrue(width < combo.Width);
+        }
+    }
+}
---
+++ SomeTools/ExtensionMethods/ComboBoxExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+
+namespace SomeTools
+{
+    public static class ComboBoxExtensions
+    {
+        public static void AutoSize(this ComboBox comboBox)
+        {
+            int current;
+            int maximum = 0;
+
+            foreach (object text in comboBox.Items)
+            {
+                current = TextRenderer.MeasureText(text.ToString(), comboBox.Font).Width;
+
+                if (current > maximum)
+                    maximum = current;
+            }
+
+            int width = maximum + SystemInformation.VerticalScrollBarWidth;
+
+            if (comboBox.Width != width)
+            { 
+                comboBox.Width = width;
+                comboBox.Refresh();
+            }
+        }
+    }
+}
