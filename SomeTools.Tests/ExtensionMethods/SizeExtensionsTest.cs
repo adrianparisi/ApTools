66dfe2a1e54c682268587c5419e595d4d57ac0f1
Adrian Parisi <adrian.parisi@gmail.com>
2016-03-19 14:51:10 -0300

    Add Divide and Multiply extension methods to Size.

---
+++ SomeTools.Tests/ExtensionMethods/SizeExtensionsTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Drawing;
+
+namespace SomeTools.Tests.ExtensionMethods
+{
+    [TestClass]
+    public class SizeExtensionsTest
+    {
+        [TestMethod]
+        public void DivisionTest()
+        {
+            Size size = new Size(10, 30);
+
+            Size expected = new Size(5, 15);
+            Size actual = size.Divide(2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivideByZeroExceptionTest()
+        {
+            Size size = new Size(10, 10);
+
+            size.Divide(0);
+        }
+
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            Size size = new Size(5, 10);
+
+            Size expected = new Size(15, 30);
+            Size actual = size.Multiply(3);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ SomeTools/ExtensionMethods/SizeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace SomeTools
+{
+    public static class SizeExtensions
+    {
+        public static Size Divide(this Size size, int divider)
+        {
+            return new Size(size.Width / divider, size.Height / divider);
+        }
+
+        public static Size Multiply(this Size size, int multiplier)
+        {
+            return new Size(size.Width * multiplier, size.Height * multiplier);
+        }
+    }
+}
